a65b8710f232f409aa5e25c0915af8db0fcb4a71
sergey-komar <kursev81@yandex.ru>
2022-09-07 23:12:43 +0300

    desctop

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,43 @@
 $(function () {
+    $('.slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-after-left"></button>',
+        nextArrow: '<button type="button" class="slick-after-rigth"></button>',
 
-})
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false
+              }
+            },
+            {
+              breakpoint: 550,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+});
+
+
