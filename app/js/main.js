a734738cadf1cff495c6f5b8cb12fd3cef1a30b9
sergey-komar <kursev81@yandex.ru>
2022-09-09 19:33:31 +0300

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -38,6 +38,29 @@ $(function () {
             // instead of a settings object
           ]
     });
+
+    const menu = document.querySelector('.menu');
+    const mobile = document.querySelector('.nav-icon');
+
+    mobile.addEventListener('click', function(){
+    this.classList.toggle('nav-icon--active');
+    menu.classList.toggle('nav--active');
+});
+
+// Находим ссылки внутри мобильной навигации
+// const navLinks = document.querySelectorAll('.menu__list a');
+const navLinks = document.querySelectorAll('.coaching__menu a');
+
+// Обходим ссылки методом forEach
+navLinks.forEach(function (item) {
+  // Для каждой ссылки добавляем прослушку по событию "Клик"
+  item.addEventListener('click', function () {
+    mobile.classList.remove('nav-icon--active'); // Убираем активный класс у иконки моб. навигации
+    menu.classList.remove('nav--active'); // Убираем активный класс у блока моб. навигации
+  });
+  
+});
+
 });
 
 
